33728e16ddb289f767a5a4ad62258a8e482a480b
flymie <984421392@qq.com>
2024-12-20 15:54:02 +0800

    :feat python

--- python/shot.py
+++ python/shot.py
@@ -11,24 +11,32 @@ import keyboard
 def click_loop():
     while True:
         if not keyboard.is_pressed('q'):  # 按下 'q' 键时停止
+            pyautogui.click(293.77734375, 608.09375)
+            print(f'微信菜单')
+            time.sleep(1)
+
+            pyautogui.click(367.9765625, 354.52734375)
+            print(f'进入游戏')
+            time.sleep(20)
+
             pyautogui.click(717.6015625, 680.98828125)
             print(f'鼠标在开始游戏位置被点击')
 
-            time.sleep(3)
+            time.sleep(1)
             print(f'进入游戏')
 
             pyautogui.click(821.46484375, 68.09765625)
             time.sleep(1)
-            print(f'菜单')
+            print(f'游戏菜单')
 
             pyautogui.click(690.2578125, 330.48046875)
-            time.sleep(20)
-            print(f'重启')
+            time.sleep(1)
+            print(f'exit')
         else:
             break
 
-            
- 
+
+
 time.sleep(5)
 print(f'游戏界面')
 click_loop()
